6ac2f25477e108fa142a6c6ee8f2efce5011c290
Lucas Meira <128614213+Meira-Lucas@users.noreply.github.com>
2024-11-14 23:02:53 -0300

    add codigo pronto

--- Zimmer/lib/Empresas.dart
+++ Zimmer/lib/Empresas.dart
@@ -22,7 +22,7 @@ class EmpresasScreen extends StatelessWidget {
       description:
           "Estúdio fundado por Hans Zimmer, que se tornou um importante centro de composição para trilhas sonoras. A empresa abriga vários compositores e músicos que colaboram em projetos de filmes e jogos.",
       logoUrl:
-          "https://upload.wikimedia.org/wikipedia/commons/4/4d/Remote_Control_Productions_logo.png",
+          "https://www.dropbox.com/scl/fi/99002ofjsi19e2w68xk7j/377647785-cf865750-49c2-40be-91cd-dbd8f53088e4-removebg-preview.png?rlkey=k1l0zd8moivcl2hz66vj8c9au&st=filzjhef&raw=1",
       websiteUrl: "https://www.remotecontrolproductions.com/",
     ),
     Company(
@@ -30,8 +30,98 @@ class EmpresasScreen extends StatelessWidget {
       description:
           "Inclui a trilogia 'Batman' de Christopher Nolan e 'Harry Potter', onde a música de Zimmer ajudou a criar a atmosfera emocional dos filmes.",
       logoUrl:
-          "https://upload.wikimedia.org/wikipedia/commons/5/5a/Warner_Bros_%28WB%29_logo.png",
+          "https://www.dropbox.com/scl/fi/f6n4qp0fck7leym3lowib/377638703-f3199a63-47af-4d40-8b4f-03c434225b30-removebg-preview.png?rlkey=t2xd3vvsgob7j677w7xhe7qgb&st=30uwkp2k&raw=1",
       websiteUrl: "https://www.warnerbros.com/",
+    ),
+    Company(
+      name: "Disney",
+      description:
+          "A trilha sonora de O Rei Leão (1994) é uma das mais conhecidas e aclamadas, tendo recebido vários prêmios, incluindo um Oscar.",
+      logoUrl:
+          "https://www.dropbox.com/scl/fi/hikc2kboroh05uhrjn9cr/377639227-da6a9342-ffde-404d-a798-3beb10778fb3.jpg?rlkey=ww2ndjwogk8r4c9dsv2uah34o&st=jtn1gfdb&raw=1",
+      websiteUrl: "https://www.disney.com.br/",
+    ),
+    Company(
+      name: "DreamWorks",
+      description:
+          "Composições para Gladiador e a série Shrek, que mescla humor e emoção.",
+      logoUrl:
+          "https://www.dropbox.com/scl/fi/nnldcei8b6e2dv5f36siq/377640929-34eaa7b7-de47-436d-b438-6f9a19186481.jpg?rlkey=b3twkym44qx0yyw6aanhxkr09&st=jkon5zxl&raw=1",
+      websiteUrl: "https://www.dreamworks.com/",
+    ),
+    Company(
+      name: "20th Century Fox",
+      description:
+          " Inclui colaborações em filmes como O Código Da Vinci e A Origem.",
+      logoUrl:
+          "https://www.dropbox.com/scl/fi/ork2uyiy1pcv11wd2f3lw/377641714-09970339-f1ba-4574-a22c-7a31943cbbf2.jpg?rlkey=1bdc49qdzwhkf6fl6h2dsisco&st=691g55l8&raw=1",
+      websiteUrl: "https://www.20thcenturystudios.com/",
+    ),
+    Company(
+      name: "Universal Pictures",
+      description:
+          "Trabalhos em trilhas sonoras para filmes de ação e aventura, incluindo O Código Da Vinci.",
+      logoUrl:
+          "https://www.dropbox.com/scl/fi/a01tj6bw7gp1hirhip7c8/377642091-f18b215a-41b1-4fed-a737-a64a0ced8f0a.jpg?rlkey=f0amry1xrpvrquj7lkjvij9ly&st=sf9tjahe&raw=1",
+      websiteUrl: "https://www.universalpictures.com/",
+    ),
+    Company(
+      name: "Columbia Pictures",
+      description:
+          "Trabalhos em filmes como A Última Fronteira e A Guerra do Fogo.",
+      logoUrl:
+          "https://www.dropbox.com/scl/fi/y4oi17ouj60g6f63h6jv4/377643295-787ffa55-f8d5-4155-87f1-27449c474403.jpg?rlkey=pigpvnmbgr595jw8fh3an0uvn&st=r0ok5waa&raw=1",
+      websiteUrl: "https://www.sonypictures.com/",
+    ),
+    Company(
+      name: "New Line Cinema",
+      description:
+          "Colaborações com outros compositores na trilha sonora dos filmes.",
+      logoUrl:
+          "https://www.dropbox.com/scl/fi/virf8950q846n8tgydihg/377645137-fe21b5f0-638f-4390-b257-07faf499fd42.png?rlkey=6wnc41sy26i80g6cdxxf26dwr&st=an5zs8zl&raw=1",
+      websiteUrl:
+          "https://www.warnerbros.com/company/divisions/motion-pictures",
+    ),
+    Company(
+      name: "Paramount Pictures",
+      description:
+          "Trabalhos em trilhas sonoras para filmes de ação e aventura.",
+      logoUrl:
+          "https://www.dropbox.com/scl/fi/xeyzbbb3asoissm5pji2z/377643869-7e236349-c27b-477f-8852-6066d66665df-removebg-preview.png?rlkey=87y6pccalac26i8xtmzsooq7x&st=k021cglc&raw=1",
+      websiteUrl: "https://www.paramountpictures.com/",
+    ),
+    Company(
+      name: "Sony Pictures Entertainment",
+      description:
+          "Trabalhos variados, incluindo trilhas sonoras para filmes de ação e drama.",
+      logoUrl:
+          "https://www.dropbox.com/scl/fi/ackkzsu8h7qz69cizavjl/377645662-099ec290-0934-4d65-bdd2-58e2bd89f6c0-removebg-preview.png?rlkey=9qytoa8xl6euunjdf0t0b031l&st=3mwrzcw4&raw=1",
+      websiteUrl: "https://www.sonypictures.com/",
+    ),
+    Company(
+      name: "Activision",
+      description:
+          "Composição para Call of Duty: Modern Warfare 2, onde sua música ajudou a intensificar a experiência de jogo.",
+      logoUrl:
+          "https://www.dropbox.com/scl/fi/pd26lq09icx57j2bch7vg/377648667-53b750a5-f262-4523-bf58-ac63fea23a5e.png?rlkey=wye5lwwrz3oua4yeomuhvtkjx&st=urphe5p9&raw=1",
+      websiteUrl: "https://www.activision.com/",
+    ),
+    Company(
+      name: "Interscope Records",
+      description:
+          "Trabalhos em trilhas sonoras e colaborações musicais com artistas",
+      logoUrl:
+          "https://www.dropbox.com/scl/fi/s0w1qp9v944rsr092o3bk/377648031-10f4312e-56b9-4a39-ba8b-21d5aeeb52cc-removebg-preview.png?rlkey=t1mo7juq7xtf3bji6b6296zi1&st=iwaz5u91&raw=1",
+      websiteUrl:
+          "https://interscope.com/?srsltid=AfmBOoqIdEWB_5OjzueeS9fC7XqtIsiz58mn_ruLwssC60k4cvwt02F7",
+    ),
+    Company(
+      name: "Ubisoft",
+      description:
+          "Participação na trilha sonora do filme Assassin’s Creed, que é baseado na popular série de jogos.",
+      logoUrl:
+          "https://www.dropbox.com/scl/fi/mpo8tvn3ufm743810cqhs/377648732-68d9b3fd-c8c0-479e-9187-dc377c0ae085-removebg-preview.png?rlkey=xuigpu3bw2gblo610fdyi4y2k&st=ch49lmyh&raw=1",
+      websiteUrl: "https://www.ubisoft.com/pt-br/",
     ),
   ];
 
@@ -52,7 +142,7 @@ class EmpresasScreen extends StatelessWidget {
         title: Text("Empresas Famosas onde passou"),
         backgroundColor: Color(0xff000000),
       ),
-      body: Column(
+      body: ListView(
         children: companies.map((company) {
           return CompanyCard(
             company: company,
--- Zimmer/lib/Musicas.dart
+++ Zimmer/lib/Musicas.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'Biography.dart';
-import 'Empresas.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(MyApp());
@@ -24,36 +23,94 @@ class Musica {
 }
 
 // Classe para a tela de músicas
-class MusicasScreen extends StatelessWidget {
-  // Lista de músicas com título e URL
+class MusicasScreen extends StatefulWidget {
+  @override
+  _MusicasScreenState createState() => _MusicasScreenState();
+}
+
+class _MusicasScreenState extends State<MusicasScreen> {
+  final AudioPlayer _audioPlayer = AudioPlayer();
+
   final List<Musica> musicas = [
     Musica(
+      titulo: 'Batman: Cavaleiro das Trevas',
+      url:
+          'https://www.dropbox.com/scl/fi/s1b77sh3hsm1or565hdwp/The-Dark-Knight-Rises-OST-1.-A-Storm-is-Coming-Hans-Zimmer-M4A_128K.m4a?rlkey=77s9iv9m3yo337dzhtyp06eol&st=j2fhlrex&raw=1',
+    ),
+    Musica(
+      titulo: 'Rango',
+      url:
+          'https://www.dropbox.com/scl/fi/bjwndvqz84xxmq47gm4m3/Rango-Suite-M4A_128K.m4a?rlkey=x9ybw55p4fl7wh5e1qo1xlj9c&st=8qyn3stu&raw=1',
+    ),
+    Musica(
+      titulo: 'Os Simpsons',
+      url:
+          'https://www.dropbox.com/scl/fi/y8p8fw4pii2ldlud2cypx/The-Simpsons-Movie-OST-_1-The-Simpsons_-television-theme-M4A_128K.m4a?rlkey=fpmurc11i5x6e42oi166ztajy&st=4wmqeim8&raw=1',
+    ),
+    Musica(
+      titulo: 'Piratas do Caribe: No Fim do Mundo',
+      url:
+          'https://www.dropbox.com/scl/fi/6r3paucwx0t2xycq6kmks/Up-Is-Down-From-_Pirates-of-the-Caribbean_-At-World_s-End__Score-M4A_128K.m4a?rlkey=whxnu6bsjrfbg547h8js33cw3&st=lp1e9jdy&raw=1',
+    ),
+    Musica(
+      titulo: 'Piratas do Caribe: O Baú da Morte',
+      url:
+          'https://www.dropbox.com/scl/fi/pupw3gk94wxazgeg2865h/Pirates-Of-The-Caribbean-Dead-Man_s-Chest-Score-01-Jack-Sparrow-Hans-Zimmer-M4A_128K.m4a?rlkey=fcr6zrr3apjcl6bo9fm69gfrb&st=yzq779pz&raw=1',
+    ),
+    Musica(
+      titulo: 'Batman Begins',
+      url:
+          'https://www.dropbox.com/scl/fi/hcr3mbtebkhtguvg6ti2n/Batman-Begins-OST-Vespertilio-M4A_128K.m4a?rlkey=3hxn66meiisoaswgo814iqwrm&st=n6tyetqm&raw=1',
+    ),
+    Musica(
       titulo: 'INTERSTELLAR',
-      url: 'https://www.example.com/interstellar.mp3',
+      url:
+          'https://www.dropbox.com/scl/fi/6py143gvu0v5rzftt4f9y/Interstellar-Main-Theme-Extra-Extended-Soundtrack-by-Hans-Zimmer-M4A_128K.m4a?rlkey=lnj59atndcng7a3ftjwr0gnvv&st=fivu0k53&raw=1',
     ),
     Musica(
-      titulo: 'PIRATAS DO CARIBE',
-      url: 'https://www.example.com/pirates.mp3',
+      titulo: 'PIRATAS DO CARIBE - Trilha Principal',
+      url:
+          'https://www.dropbox.com/scl/fi/dh67b8j9zwghbg1iu5r6s/Main-Theme-_-Pirates-of-the-Caribbean-M4A_128K.m4a?rlkey=4tl8fs3w1ri7u7f1mfllwvxgn&st=w8jvffqw&raw=1',
     ),
     Musica(
-      titulo: 'GLADIADOR',
-      url: 'https://www.example.com/gladiator.mp3',
+      titulo: 'O Chamado',
+      url:
+          'https://www.dropbox.com/scl/fi/zqfgrnbpebn9hb3wpq0cz/O-Chamado-Trailer-Original-M4A_128K.m4a?rlkey=p5vv98i0s581mv4i6vsu1e2q6&st=jvpblf10&raw=1',
     ),
     Musica(
-      titulo: 'A ORIGEM',
-      url: 'https://www.example.com/inception.mp3',
+      titulo: 'Pear Harbor',
+      url:
+          'https://www.dropbox.com/scl/fi/e5rqwnperh5ef86s77zw2/Pearl-Harbor-Soundtrack-Tennessee-Hans-Zimmer-M4A_128K.m4a?rlkey=dpknna3qajb8h3i7euuuje73h&st=jn70j025&raw=1',
     ),
     Musica(
-      titulo: 'O REI LEÃO',
-      url: 'https://www.example.com/lionking.mp3',
+      titulo: 'Gladiador',
+      url:
+          'https://www.dropbox.com/scl/fi/u60x8cumw6llxf31nw0wh/Gladiator-Now-We-Are-Free-Super-Theme-Song-M4A_128K.m4a?rlkey=86eap58fzhqirmp1sw79ztnth&st=w8zg1wb5&raw=1',
+    ),
+    Musica(
+      titulo: 'Rei Leão',
+      url:
+          'https://www.dropbox.com/scl/fi/nucosvdhdyl2pd3iendwi/Lion-King-Complete-Score-01-Circle-Of-Life-Hans-Zimmer-M4A_128K.m4a?rlkey=ec6dg1okw0u1zmxp3n7yooys6&st=43c2alw7&raw=1',
     ),
   ];
+
+  // Função para reproduzir a música
+  void _playMusic(String url) async {
+    await _audioPlayer.stop(); // Parar qualquer áudio em execução
+    await _audioPlayer.play(url); // Reproduzir diretamente com o URL
+  }
+
+  @override
+  void dispose() {
+    _audioPlayer.dispose(); // Liberar recursos do player ao sair
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Trilhas'),
+        title: Text('Músicas'),
         backgroundColor: Colors.black,
       ),
       body: Stack(
@@ -72,10 +129,10 @@ class MusicasScreen extends StatelessWidget {
             itemBuilder: (context, index) {
               final musica = musicas[index];
               return ListTile(
-                leading: Image.network(
-                  'https://www.example.com/album_cover_${index + 1}.jpg',
-                  width: 50,
-                  height: 50,
+                leading: Icon(
+                  Icons.music_note,
+                  color: Colors.white,
+                  size: 40,
                 ),
                 title: Text(
                   musica.titulo,
@@ -86,51 +143,12 @@ class MusicasScreen extends StatelessWidget {
                   color: Colors.white,
                 ),
                 onTap: () {
-                  // Código para tocar a música a partir da URL
-                  // Pode ser implementado com um plugin como 'audioplayers'
+                  _playMusic(musica.url); // Reproduzir a música ao clicar
                 },
               );
             },
           ),
         ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.black,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home, color: Colors.white),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person, color: Colors.white),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.album, color: Colors.white), // Ícone de disco
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business, color: Colors.white),
-            label: '',
-          ),
-        ],
-        onTap: (index) {
-          // Navegação entre as telas de acordo com o índice do ícone
-          switch (index) {
-            case 1:
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => BiographyScreen()),
-              );
-              break;
-            case 2:
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => EmpresasScreen()),
-              );
-              break;
-          }
-        },
       ),
     );
   }
